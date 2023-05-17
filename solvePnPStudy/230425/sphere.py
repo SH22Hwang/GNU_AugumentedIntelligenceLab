0cb5eac12534ced7e391362797f20af989510067
SEUNGHYEON HWANG <ghkd3531@gnu.ac.kr>
2023-05-17 15:40:14 +0900

    230517 세미나

--- solvePnPStudy/230425/sphere.py
+++ solvePnPStudy/230425/sphere.py
@@ -1,29 +1,73 @@
-import cv2
-import numpy as np
-from OpenGL.GL import *
+from OpenGL.GLUT import *
 from OpenGL.GLU import *
-from OpenGL.GLUT import glutSolidSphere, glutInit, glutInitDisplayMode, glutCreateWindow, GLUT_DOUBLE, GLUT_RGB, GLUT_DEPTH
+from OpenGL.GL import *
+import sys
+from PIL import Image as Image
+import numpy
 
-glutInit()
-glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-glutCreateWindow("OpenGL")
+name = 'Navigation paradigm'
 
-cv2.namedWindow("OpenGL")
-cv2.imshow("OpenGL", np.zeros((600, 600, 3), dtype=np.uint8))
-cv2.waitKey(1)
+def main():
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(800, 800)
+    glutInitWindowPosition(350, 200)
+    glutCreateWindow(name)
 
-while True:
+    glClearColor(0., 0., 0., 1.)
+    glShadeModel(GL_SMOOTH)
+    glEnable(GL_CULL_FACE)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_LIGHTING)
+
+    lightZeroPosition = [10., 4., 10., 1.]
+    lightZeroColor = [0.8, 1.0, 0.8, 1.0]
+    glLightfv(GL_LIGHT0, GL_POSITION, lightZeroPosition)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightZeroColor)
+    glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.1)
+    glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.05)
+    glEnable(GL_LIGHT0)
+
+    glutDisplayFunc(display_scene)
     glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45, 1, 0.1, 50.0)
-
+    gluPerspective(40., 1., 1., 40.)
     glMatrixMode(GL_MODELVIEW)
-    glLoadIdentity()
-    gluLookAt(0, 0, 10, 0, 0, 0, 0, 1, 0)
+    gluLookAt(0, 0, 10,
+              0, 0, 0,
+              0, 1, 0)
+    glPushMatrix()
+    glutMainLoop()
+    return
 
+def display_scene():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glColor3f(1.0, 1.0, 1.0)
-    glutSolidSphere(1.0, 20, 20)
+    glPushMatrix()
+    # Textured thing 
+    # tex = read_texture('brick.jpg')
+    qobj = gluNewQuadric()
+    gluQuadricTexture(qobj, GL_TRUE)
+    glEnable(GL_TEXTURE_2D)
+    # glBindTexture(GL_TEXTURE_2D, tex)
+    glBegin(GL_TRIANGLES)
+    gluSphere(qobj, 1, 50, 50)
+    gluDeleteQuadric(qobj)
+    glDisable(GL_TEXTURE_2D)
 
-    cv2.imshow("OpenGL", np.zeros((600, 600, 3), dtype=np.uint8))
-    cv2.waitKey(1)
+    # Left sphere
+    color = [1.0, 0.0, 0.0, 1.0]
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, color)
+    glTranslatef(-2, 0, 0)
+    glutSolidSphere(1, 100, 20)
+
+    # Right sphere
+    color = [0.0, 1.0, 0.0, 1.0]
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, color)
+    glTranslatef(4, 0, 0)
+    glutSolidSphere(1, 100, 20)
+
+    glPopMatrix()
+    glutSwapBuffers()
+    return
+
+if __name__ == '__main__':
+    main()
